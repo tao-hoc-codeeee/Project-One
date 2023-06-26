5cb11ba752151e301051cce56781c140005dd3c5
Dris305 <nguyenthequan305@gmail.com>
2023-06-26 23:28:16 +0700

    add check gender and phone number

--- Addinfostudents.cs
+++ Addinfostudents.cs
@@ -18,28 +18,38 @@ public class Addinfostudent : Studentsinfo
     }
     public void Addstudent()
     {
+        //nhap ten
         Console.WriteLine("Enter name:");
         name_of_student = Console.ReadLine()??"";
+        
+        //nhap email
         Console.Write("Enter email_address:");
         string email = Console.ReadLine()??"";
-        int i = chekcmail(email);
+        int i = chekcmail(email); // check email
         while(i!=1)
         {
             Console.WriteLine("Wrong email format");
         }
         email = students_mail;
-        Console.Write("Enter phonenumber:");
-        students_phonenumber = Console.ReadLine()??"";
-        Console.Write("Enter gender (enter F or M):");
-        string sexual = Console.ReadLine()??"";
-        students_sex = Gender(sexual);
+
+        //nhap so dien thoai
+        students_phonenumber = Number_Phone();
+
+        //nhap goi tinh
+        students_sex = Gender();
+        
+        //nhap nam sinh
         Console.Write("Enter birthday:");
         year = Convert.ToInt32(Console.ReadLine());
         day = Convert.ToInt32(Console.ReadLine());
         month = Convert.ToInt32(Console.ReadLine());
         students_birth = new DateTime(year, month, day);
+        
+        //nhap email
         Console.Write("Enter address:");
         studentsaddress = Console.ReadLine()??"";
+
+        //hien thi lai thong tin cho nguoi dung xac minh lai 1 lam nua
         Console.WriteLine("Information :");
         Console.WriteLine("Name : {0}", name_of_student);
         Console.WriteLine("ID :", studentsno);
@@ -61,15 +71,41 @@ public class Addinfostudent : Studentsinfo
             return 0;
         }
     }
-    public string Gender(string gender)
+    public string Gender()
     {
-        if(gender == "F" && gender == "f")
+        while(true)
         {
-            return genderfemale;
-        }
-        else
-        {
-            return gendermale;
+            Console.Write("Enter gender (enter F or M):");
+            string gender = Console.ReadLine()??"".ToUpper();
+            if(gender == "F" || gender == "M")
+            {
+                return gender;
+            }
+            else
+            {
+                Console.WriteLine("Invalid gender. Please re-enter.");
+            }
         }
     }
+
+    public string Number_Phone()
+    {
+        while(true)
+        {
+            Console.Write("Enter phonenumber:");
+            string Phonenumber = Console.ReadLine()??"";
+            int length = Phonenumber.Length;
+
+            if(length == 10 || length == 11)
+            {
+                return Phonenumber;
+            }
+            else
+            {
+                Console.WriteLine("Invalid phone number format. Please enter again!");
+            }
+        }
+    }
+
+    
 }
