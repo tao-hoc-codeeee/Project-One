0dca7d99d6e69b92260b3466dd6f1128f88be2ad
Dris305 <nguyenthequan305@gmail.com>
2023-06-27 23:57:38 +0700

    update login 1

--- Connect&Login.cs
+++ Connect&Login.cs
@@ -27,24 +27,50 @@ public class LoginUI
     public string? ID; 
     public void Login()
     {
+        Console.WriteLine("=====================================");
         Console.WriteLine("\t\t\t\tLogin\t\t\t\t");
+        Console.WriteLine("=====================================");
         Console.Write("Enter your ID :");
         ID = Console.ReadLine()??"";
         Console.Write("Enter your password :");
+        string password = ReadPassword();
+
+        Console.WriteLine("Đăng nhập thành công:");
+        Console.WriteLine("Tên đăng nhập: " + ID);
+        Console.WriteLine("Mật khẩu: " + password);
+
+        Console.ReadLine();
+    }
+
+    static string ReadPassword()
+    {
+        string password = "";
         ConsoleKeyInfo key;
+
         do
         {
             key = Console.ReadKey(true);
-            if(key.Key != ConsoleKey.Backspace)
+
+            // Nếu phím không phải là Enter, xử lý xóa ký tự
+            if (key.Key != ConsoleKey.Enter)
             {
-                pass += key.KeyChar;
-                Console.Write("*");
+                if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    // Xóa ký tự cuối cùng trong mật khẩu và di chuyển con trỏ về trước
+                    password = password.Substring(0, password.Length - 1);
+                    Console.Write("\b \b");
+                }
+                else if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Delete)
+                {
+                    // Thêm ký tự vào mật khẩu và hiển thị dấu *
+                    password += key.KeyChar;
+                    Console.Write("*");
+                }
             }
-            else
-            {
-                Console.Write("\b");
-            }
-        }
-        while (key.Key != ConsoleKey.Enter);
+
+        } while (key.Key != ConsoleKey.Enter);
+
+        Console.WriteLine(); // Xuống dòng sau khi người dùng nhấn Enter
+        return password;
     }
 }
